68cc8a8d70945a9c30c466e56121a5da8e6e8c36
Esha Eman <eshe284@gmail.com>
2026-08-14 23:30:49 +0500

    fix: correct OpportunityEdit import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-// Pages
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
@@ -11,8 +10,8 @@ import NGORegister from "./Pages/NGORegister";
 import NGOProfile from "./Pages/NGOProfile";
 import Opportunities from "./Pages/Opportunities";
 import OpportunityDetail from "./Pages/OpportunityDetail";
-import CreateOpportunity from "./Pages/CreateOpportunity";        // ← Fixed
-import EditOpportunity from "./Pages/EditOpportunity";            // ← Fixed (agar ye naam hai)
+import CreateOpportunity from "./Pages/CreateOpportunity";
+import OpportunityEdit from "./Pages/OpportunityEdit";
 import VolunteerProfile from "./Pages/VolunteerProfile";
 import AdminDashboard from "./Pages/AdminDashboard";
 import ComplaintForm from "./Pages/ComplaintForm";
@@ -26,27 +25,17 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        {/* Volunteer Routes */}
         <Route path="/volunteer/dashboard" element={<VolunteerDashboard />} />
         <Route path="/volunteer/profile" element={<VolunteerProfile />} />
-        
-        {/* NGO Routes */}
         <Route path="/ngo/dashboard" element={<NGODashboard />} />
         <Route path="/ngo/register" element={<NGORegister />} />
         <Route path="/ngo/profile" element={<NGOProfile />} />
-        
-        {/* Opportunities */}
         <Route path="/opportunities" element={<Opportunities />} />
         <Route path="/opportunity/:id" element={<OpportunityDetail />} />
-        <Route path="/opportunity/create" element={<CreateOpportunity />} />   // ← Fixed
-        <Route path="/opportunity/edit/:id" element={<EditOpportunity />} />    // ← Fixed
-        
-        {/* Other */}
+        <Route path="/opportunity/create" element={<CreateOpportunity />} />
+        <Route path="/opportunity/edit/:id" element={<OpportunityEdit />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/complaint" element={<ComplaintForm />} />
-        
-        {/* 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
